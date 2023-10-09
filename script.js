5348a1e4c8cb1d8f552e61e523e6d0d9dc3dc08b
Steff <steff.the.d3v@gmail.com>
2023-10-09 20:34:55 +0100

    updated commit

--- script.js
+++ script.js
@@ -1,45 +1,115 @@
-//toggle icon navbar
-let menuIcon = document.querySelector(" #menu-icon");
+// //toggle icon navbar
+// let menuIcon = document.querySelector(" #menu-icon");
+// let navbar = document.querySelector(".navbar");
+
+// menuIcon.onclick = () => {
+//   menuIcon.classList.toggle("bx-x");
+//   navbar.classList.toggle("active");
+// };
+// // scroll sections
+// let sections = document.querySelectorAll("section");
+// let navLinks = document.querySelectorAll("header nav a");
+
+// window.onscroll = () => {
+//   sections.forEach((sec) => {
+//     let top = window.scrollY;
+//     let offset = sec.offsetTop - 100;
+//     let height = sec.offsetHeight;
+//     let id = sec.getAttribute("id");
+
+//     if (top >= offset && top < offset + height) {
+//       // active navbar links
+//       navLinks.forEach((links) => {
+//         links.classList.remove("active");
+//         document
+//           .querySelector("header nav a[href*=" + id + "]")
+//           .classList.add("active");
+//       });
+//       // active sections for animation on scrollxx`
+//       sec.classList.add("show-animate");
+//     }
+//     // if want to use animation that repeats on scroll use this
+//     else {
+//       sec.classList.remove("show-animate");
+//     }
+//   });
+
+//   // sticky header
+//   let header = document.querySelector("header");
+
+//   header.classList.toggle("sticky", window.scrollY > 100);
+
+//   // remove toggle icon and navbar when click navbar link (scroll)
+//   menuIcon.classList.remove("bx-x");
+//   navbar.classList.remove("active");
+// };
+
+/=======togglee icon media screen==========/;
+let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
+  menuIcon.classList.toggle("bx-message-square-x");
   navbar.classList.toggle("active");
 };
-// scroll sections
+
+/=======for active links web layout==========/;
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
   sections.forEach((sec) => {
     let top = window.scrollY;
-    let offset = sec.offsetTop - 100;
+    let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      // active navbar links
       navLinks.forEach((links) => {
         links.classList.remove("active");
         document
           .querySelector("header nav a[href*=" + id + "]")
           .classList.add("active");
       });
-      // active sections for animation on scrollxx`
-      sec.classList.add("show-animate");
-    }
-    // if want to use animation that repeats on scroll use this
-    else {
-      sec.classList.remove("show-animate");
     }
   });
 
-  // sticky header
+  /=======sticky navbar==========/;
   let header = document.querySelector("header");
 
   header.classList.toggle("sticky", window.scrollY > 100);
 
-  // remove toggle icon and navbar when click navbar link (scroll)
-  menuIcon.classList.remove("bx-x");
+  /=======to rempve the open burger-menu when a link is clicked or we scroll==========/;
+  menuIcon.classList.remove("bx-message-square-x");
   navbar.classList.remove("active");
 };
+
+/=======scroll reveal==========/;
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .about-img, .skills-container, .portfolio-box, .central",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(
+  ".home-content h1, .about-content h2, .about-content h3",
+  { origin: "left" }
+);
+ScrollReveal().reveal(".about-content h2, .about-content h3", {
+  origin: "right",
+});
+
+/=======typed.js text==========/;
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Graphics Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
